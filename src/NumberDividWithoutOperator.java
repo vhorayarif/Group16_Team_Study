417e77a1c59a488a541a519a8e3c6cb601cf4f87
fnuam <amirullahomari44@gmail.com>
2024-01-31 00:09:51 -0800

    Set divide without / operator

---
+++ src/NumberDividWithoutOperator.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class NumberDividWithoutOperator {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter first no");
+        int a = sc.nextInt();
+        System.out.println("enter second no");
+        int b = sc.nextInt();
+
+        int count = 0;
+        while(a >= b){
+            a = a-b;
+            count = count+1;
+
+        }
+        System.out.println("Divide is ::" + count);
+
+
+    }
+
+}
+
+/*
+Number -- Divide without / operator
+
+Write a method that can divide two numbers without using division operator
+ */
