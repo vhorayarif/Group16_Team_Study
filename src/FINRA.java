9125d38509b0d197e8c7c54816930795bc5e9f70
ayaal <ayaalnajjar.97@gmail.com>
2024-01-30 23:53:06 -0600

    FINRA

---
+++ src/FINRA.java
@@ -0,0 +1,24 @@
+public class FINRA {
+    public static void main(String[] args) {
+        FINRA();
+
+    }
+
+    public static void FINRA() {
+
+        for (int i = 1; i <= 30; ++i) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FINRA");
+            } else if (i % 3 == 0) {
+                System.out.println("FIN");
+
+            } else if (i % 5 == 0) {
+                System.out.println("RA");
+
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
