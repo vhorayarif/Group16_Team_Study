257b0487aac3503d1d2a918110e12468537608db
masoom <masoom.timor@gmail.com>
2024-01-31 00:19:38 -0600

    set up divide without opereter

---
+++ src/DivideWithoutOperator.java
@@ -0,0 +1,25 @@
+public class DivideWithoutOperator { //n = 2, m= 1
+    public static void main (String[] args){
+        int a = divide(100,10);
+        System.out.println(a);
+    }
+
+    public static int divide(int n ,int m) {
+        int count = 0;
+        if(n >= m && m !=0){
+            if(n >= m ){
+                count ++;
+            }
+            n = n -m;
+        }
+        {
+
+        }
+
+    return count;
+}
+
+
+    {
+    }
+}
