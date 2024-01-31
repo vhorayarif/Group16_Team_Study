32c01b70247032e357cebcdf4a953279ccbf4285
faridkarimov <kefarid@yahoo.com>
2024-01-30 21:24:36 -0600

    3rd one

---
+++ src/FinRa.java
@@ -0,0 +1,19 @@
+public class FinRa {
+        public static void main(String[] args) {
+            printFINRA();
+        }
+
+        public static void printFINRA() {
+            for (int i = 1; i <= 30; i++) {
+                if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.println("FINRA");
+                } else if (i % 3 == 0) {
+                    System.out.println("FIN");
+                } else if (i % 5 == 0) {
+                    System.out.println("RA");
+                } else {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
