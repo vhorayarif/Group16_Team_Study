5cbce510c1c944bf148a42428c214e07d7ff469b
ayaal <ayaalnajjar.97@gmail.com>
2024-01-30 23:53:23 -0600

    Even or Odd.

---
+++ src/evenOrOdd.java
@@ -0,0 +1,15 @@
+public class evenOrOdd {
+    public static void main(String[] args) {
+
+    }
+
+    public static String oddOrEven(int num) {
+        String oddOrEven = "";
+        if (num % 2 == 0) {
+            oddOrEven += "Even";
+        } else {
+            oddOrEven += "Odd";
+        }
+        return oddOrEven;
+    }
+}
