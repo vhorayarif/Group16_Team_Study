ce4acd855160efb79cb7fa408acc8ebfc6339cb0
elif <elifkaraal@gmail.com>
2024-01-30 22:02:17 -0600

    elif - DivideWithoutOperator task solution

---
+++ src/DivideWithoutOperator.java
@@ -0,0 +1,30 @@
+public class DivideWithoutOperator {
+    public static void main(String[] args) {
+
+
+        divide(5,8);
+
+    }
+
+    public static void divide(int n1, int n2) {
+
+        if (n2 == 0) {
+            System.out.println("invalid divisor");
+            System.exit(1);
+        }
+
+
+        int count = 0;
+
+        while (n1 >= n2) {
+            count++;
+            n1 -= n2;
+        }
+
+        System.out.println(count);
+
+
+    }
+
+}
+
