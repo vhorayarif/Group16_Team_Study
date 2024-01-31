a8d4168b40f382d84b70b5558fa4d59343b10f39
masoom <masoom.timor@gmail.com>
2024-01-30 23:36:45 -0600

    Odd and Even number created

---
+++ src/OddOrEven.java
@@ -0,0 +1,34 @@
+public class OddOrEven {
+
+
+   public static void main (String[] args){
+
+       System.out.println(identify(5));
+
+
+    }
+
+
+    public static String identify (int num){
+       if(num % 2 == 0){
+           return "even";
+
+       }else{
+           return "Odd";
+       }
+    }
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+  //  write a method which can identifies given number is even or odd
