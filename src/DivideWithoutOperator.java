edaa9d1572aeaea81378b762599b05a187ccac9e
ayaal <ayaalnajjar.97@gmail.com>
2024-01-31 20:31:33 -0600

    Divide without operator

--- src/DivideWithoutOperator.java
+++ src/DivideWithoutOperator.java
@@ -1,10 +1,13 @@
 public class DivideWithoutOperator {
     public static void main(String[] args) {
-        System.out.println(Result(144,12));
+        System.out.println(Result(144, 12));
 
     }
 
     public static int Result(int n, int m) {
+        if (m == 0) {
+            throw new RuntimeException("Division by zero is not allowed ");
+        }
         int count = 0;
         while (n >= m) {
             n -= m;
