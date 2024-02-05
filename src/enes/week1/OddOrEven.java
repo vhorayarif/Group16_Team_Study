8516d0236b05a0693f109f721a82270114f09e98
Ebal2023 <es.bal@outlook.com>
2024-02-05 16:59:19 -0600

    Enes solution added - OddOrEven problem

--- src/enes/week1/OddOrEven.java
+++ src/enes/week1/OddOrEven.java
@@ -2,8 +2,36 @@ package enes.week1;
 
 public class OddOrEven {
 
-    /*
-    * write a method
-    *
-    * */
+    public static void main(String[] args) {
+
+        System.out.println(isEvenOrOdd(1245));
+        System.out.println(isEvenOrOdd(-1242));
+
+    }
+
+    public static String isEvenOrOdd(int num) {
+
+
+        int n = num;
+
+        String evenOrOdd;
+
+        boolean isEven = false;
+
+        if (n % 2 == 0) {
+
+            isEven = true;
+        }
+
+        if (isEven) {
+
+            evenOrOdd = "Even";
+        } else {
+
+            evenOrOdd = "Odd";
+        }
+
+        return evenOrOdd;
+    }
+
 }
