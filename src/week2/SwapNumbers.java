e128326066249d8951fa9e9831259adde12b6872
faridkarimov <kefarid@yahoo.com>
2024-02-06 17:43:19 -0600

    4th problem swap them numbers

--- src/week2/SwapNumbers.java
+++ src/week2/SwapNumbers.java
@@ -1,8 +1,20 @@
 package week2;
 
 public class SwapNumbers {
-}
+    public static void main(String[] args) {
 
+        int a = 4;
+        int b = 10;
+
+        a = a + b; // 14
+        b = a - b; // 4
+        a = a - b;
+
+        System.out.println("a " + a);
+        System.out.println("b " + b);
+
+    }
+}
 /*
 *Numbers -- Swap Numbers
 Swap two variable' values without using a third variable
