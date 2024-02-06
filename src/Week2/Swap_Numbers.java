cbd8c93044b88c4f84f1f89977af9fc587fd1f3d
fnuam <amirullahomari44@gmail.com>
2024-02-06 09:39:43 -0800

    Created Swap_numbers

---
+++ src/Week2/Swap_Numbers.java
@@ -0,0 +1,43 @@
+package Week2;
+
+public class Swap_Numbers {
+
+    public static void main(String[] args) {
+
+        int a  = 4;
+        int b = 10;
+
+        a = a + b; // 14
+        b = a - b; // 4
+
+        a = a - b;
+
+        System.out.println("a " + a);
+        System.out.println("b " + b);
+
+
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Swap Two Numbers
+
+Create a program that will swap two variables' values
+    Do it without a third variable
+
+ */
