e6e6f918651943b39b6483c1d387498368ce9b1c
Ebal2023 <es.bal@outlook.com>
2024-02-05 19:00:43 -0600

    Enes revised solution - SwapNumbers

--- src/enes/week2/SwapNumbers.java
+++ src/enes/week2/SwapNumbers.java
@@ -1,23 +1,60 @@
 package enes.week2;
 
+import java.util.Scanner;
+
 public class SwapNumbers {
     public static void main(String[] args) {
 
-        int x = 3;
-        int y = 11;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the first number:");
+        int x = scan.nextInt();
+        System.out.println("Please enter the second number");
+        int y = scan.nextInt();
 
-        y = y + x;
+        if (x > 0 && y > 0) {
 
-        x = y-x;
+            y = y + x;
 
-        y -= x;
+            x = y - x;
 
-        System.out.println(x);
-        System.out.println(y);
+            y -= x;
+
+            System.out.println("First number has been successfully swapped, the new value is: " + x);
+            System.out.println("Second number has been successfully swapped, the new value is: " + y);
+
+        }
 
 
+        if (x < 0 && y < 0) { // x = -3 & y = - 11;
+
+            y = y + x;
+
+            x = y - x;
+
+            y -= x;
+
+            System.out.println("First number has been successfully swapped, the new value is: " + x);
+            System.out.println("Second number has been successfully swapped, the new value is: " + y);
+
+        }
 
 
+        if (x < 0 || y < 0) { // x = -3 & y = 5;
+
+            y = -y;
+
+            y = y + x;
+
+            x = y - x;
+
+            y -= x;
+
+            x = -x;
+
+            System.out.println("First number has been successfully swapped, the new value is: " + x);
+            System.out.println("Second number has been successfully swapped, the new value is: " + y);
+
+        }
 
 
     }
