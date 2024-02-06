cebdc5b3fc848a94d1e680895d02fb4a9b314bc9
Ebal2023 <es.bal@outlook.com>
2024-02-05 18:48:14 -0600

    Enes solution - SwapNumbers

--- src/enes/week2/SwapNumbers.java
+++ src/enes/week2/SwapNumbers.java
@@ -1,6 +1,28 @@
 package enes.week2;
 
 public class SwapNumbers {
+    public static void main(String[] args) {
+
+        int x = 3;
+        int y = 11;
+
+        y = y + x;
+
+        x = y-x;
+
+        y -= x;
+
+        System.out.println(x);
+        System.out.println(y);
+
+
+
+
+
+
+    }
+
+
 }
 
 /*
