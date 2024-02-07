b1186f288cfb80e20fc17e255b8e4bdf77c9017b
ayaal <ayaalnajjar.97@gmail.com>
2024-02-06 19:59:44 -0600

    Swap Numbers

---
+++ src/Week2/SwapNumbers.java
@@ -0,0 +1,19 @@
+package Week2;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+
+    }
+
+    public static void Swap(int a, int b) {
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+
+    }
+}
+/*
+Swap two variables values without third variable.
+
+ */
