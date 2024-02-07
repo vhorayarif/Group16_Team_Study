901f55a34d026ced9e7536794be6aef2083940ea
ayaal <ayaalnajjar.97@gmail.com>
2024-02-06 20:02:45 -0600

    Consecutive numbers.

---
+++ src/Week2/ConsecutiveNumbers.java
@@ -0,0 +1,37 @@
+package Week2;
+
+import java.util.Scanner;
+
+public class ConsecutiveNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number ");
+        int num = scan.nextInt();
+        ConsecutiveNumbers(num);
+    }
+
+    public static void ConsecutiveNumbers(int n) {
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0) {
+                System.out.println("CodilityTestCoders");
+            } else if (i % 2 == 0 && i % 3 == 0) {
+                System.out.println("CodilityTest");
+            } else if (i % 2 == 0 && i % 5 == 0) {
+                System.out.println("CodilityCoders");
+            } else if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("TestCoders");
+            } else if (i % 2 == 0) {
+                System.out.println("Codility");
+
+            } else if (i % 3 == 0) {
+                System.out.println("Test");
+            } else if (i % 5 == 0) {
+                System.out.println("Coders");
+            } else {
+                System.out.println(i);
+            }
+
+
+        }
+    }
+}
