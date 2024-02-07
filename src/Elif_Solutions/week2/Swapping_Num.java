fcc83faa2cb29e95e4f3fff8148bc4c6e0d717fc
elif <elifkaraal@gmail.com>
2024-02-06 19:58:10 -0600

    elif - added week2 solutions

---
+++ src/Elif_Solutions/week2/Swapping_Num.java
@@ -0,0 +1,18 @@
+package Elif_Solutions.week2;
+
+public class Swapping_Num {
+    public static void main(String[] args) {
+
+        int x = 5;
+        int y = 10;
+
+
+        System.out.println("Before swapping x = " + x + " and y = " + y);
+
+        x = x + y;
+        y = x - y;
+        x = x - y;
+
+        System.out.println("After swapping x = " + x + " and y = " + y);
+    }
+}
